bad0f0478ddf3b3dcfbf2b2d4bd8fc1d916aacaf
isahua16 <isaelhuard@gmail.com>
2023-03-31 14:32:40 -0600

    Add comments to checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,11 +1,13 @@
+//This function first deletes the parent html element of the button that was clicked and its contents. This will remove the product from the page
 function remove_item(event)
 {
     event[`target`][`parentElement`].remove();
-    
+    //Look at all the elements in the cart array, and find a match based on the button's id attribute and the id key of each product object.
     for(let i = 0; i < cart.length; i++)
     {
         if(event[`target`].getAttribute(`product_id`) === cart[i][`id`])
         {
+            //Remove that element from the array and reset the the new cookie, and stop the loop.
             cart.splice(i, 1);
             cart_json = JSON.stringify(cart);
             Cookies.set(`cart_content`, cart_json);
@@ -14,21 +16,25 @@ function remove_item(event)
     }
 }
 
+//Remove the contents of the product_display html element that contains all the articles in the cart, and remove the cookie.
 function empty_cart(event)
 {
     product_display.remove();
     Cookies.remove(`cart_content`);
 }
 
-
+//Assign the contents of the Cookie regardless of if it exists of not
 let cart_json = Cookies.get(`cart_content`);
-let cart = 0;
+//Declare an empty cart array
+let cart = [];
 let product_display = document.querySelector(`#product_selection`);
 
+//Again, if the cookie contents is not undefined and has contents, put those elements inside cart as an array of objects.
 if(cart_json !== undefined)
 {
     cart = JSON.parse(cart_json);
 
+    //For every element in the cart array, insert the html of the selected products.
     for(let i = 0; i < cart.length; i++)
     {
         product_display.insertAdjacentHTML(`beforeend`,
@@ -44,12 +50,15 @@ if(cart_json !== undefined)
 }
 else
 {
+    //If the cart is empty, insert a message that says that the cart is empty.
     product_display.insertAdjacentHTML(`beforebegin`, `<h3 style="justify-self: center;">You have no products in your cart</h3>`)
 }
 
+//Add an event listener to the delete all button
 let empty_button = document.querySelector(`#empty_button`);
 empty_button.addEventListener(`click`, empty_cart);
 
+//Add event listeners to all the remove single element buttons.
 let delete_buttons = document.querySelectorAll(`.delete_button`)
 {
     for(let i = 0; i < delete_buttons.length; i++)
