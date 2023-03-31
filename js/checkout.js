f36bef0c9d442634e60c6e78b4abe28be398c262
isahua16 <isaelhuard@gmail.com>
2023-03-30 22:04:12 -0600

    Added deleting functionality

--- js/checkout.js
+++ js/checkout.js
@@ -1,5 +1,29 @@
+function remove_item(event)
+{
+    event[`target`][`parentElement`].remove();
+    
+    for(let i = 0; i < cart.length; i++)
+    {
+        if(event[`target`].getAttribute(`product_id`) === cart[i][`id`])
+        {
+            cart.splice(i, 1);
+            cart_json = JSON.stringify(cart);
+            Cookies.set(`cart_content`, cart_json);
+            break;
+        }
+    }
+}
+
+function empty_cart(event)
+{
+    event[`target`].remove();
+    product_display.remove();
+    Cookies.remove(`cart_content`);
+}
+
+
 let cart_json = Cookies.get(`cart_content`);
-let cart = [];
+let cart = 0;
 let product_display = document.querySelector(`#product_selection`);
 
 if(cart_json !== undefined)
@@ -8,19 +32,31 @@ if(cart_json !== undefined)
 
     for(let i = 0; i < cart.length; i++)
     {
-        product_display.insertAdjacentHTML(`beforebegin`,
+        product_display.insertAdjacentHTML(`beforeend`,
         `<article class="product_card">
             <img class="product_image" src="${cart[i][`image_url`]}" alt="Image of ${cart[i][`name`]}">
             <h3 class="product_name">${cart[i][`name`]}</h3>
             <p class="product_price">$ ${cart[i][`price`]}</p>
             <p class="product_description">${cart[i][`description`]}</p>
-            <button class="delete_button">REMOVE</button>
+            <button product_id="${cart[i][`id`]}" class="delete_button">REMOVE</button>
         </article>`);
+        
     }
-
+    product_display.insertAdjacentHTML(`afterend`, 
+    `<button id="empty_button">EMPTY CART</button>`);
+    let empty_button = document.querySelector(`#empty_button`);
+    empty_button.addEventListener(`click`, empty_cart);
 }
 else
 {
     product_display.insertAdjacentHTML(`afterbegin`, `<h3>You have no products in your cart</h3>`)
+}
+
+let delete_buttons = document.querySelectorAll(`.delete_button`)
+{
+    for(let i = 0; i < delete_buttons.length; i++)
+    {
+        delete_buttons[i].addEventListener(`click`, remove_item);
+    }
 }
 
