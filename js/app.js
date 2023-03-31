dca4dcc2737f3f6b11ac2e1967036751a2024bad
isahua16 <isaelhuard@gmail.com>
2023-03-30 20:32:19 -0600

    Created my producst

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 let products = [
     {
-        
+        name: `Bella Velvet Sofa `,
+        price: 999,
+        description: `This luxurious sofa features a soft velvet upholstery in a rich shade of navy blue. The deep seat and plush cushions offer maximum comfort, while the elegant curved arms and solid wood legs add a touch of sophistication to any living room.`,
+        id: 001,
+        image_url: `/images/velvetsofa.jpg`
+    },
+    {
+        name: `Oslo Queen Bed`,
+        price: 1499,
+        description: `Crafted from solid oak wood, this modern platform bed features a sleek and minimalistic design. The low profile frame and headboard give the bed a contemporary look, while the natural wood finish adds warmth and character to the bedroom.`,
+        id: 002,
+        image_url: `/images/oakbed.jpg`
+    },
+    {
+        name: `Remy Accent Chair`,
+        price: 399,
+        description: `Add a pop of color to any space with this chic accent chair. The Remy chair features a bold geometric pattern in shades of yellow, grey, and white. The compact size and comfortable seat make it perfect for a reading nook, bedroom, or small living room.`,
+
     }
-]
+];
+
